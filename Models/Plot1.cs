0be94b0bc3e9740e2bfb5687f75c121512c34bb0
Frank Wedlick <jfwedlick@gmail.com>
2024-03-17 14:01:43 -0400

    backend done!

--- Models/Plot1.cs
+++ Models/Plot1.cs
@@ -1,7 +1,10 @@
-﻿namespace VictoryGarden_BackEnd.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VictoryGarden_BackEnd.Models
 {
     public class Plot1
     {
+        [Key]
         public int ID { get; set; }
         public int PlantID { get; set; }
         public string PlantName { get; set; }
---
+++ Models/PostPlot.cs
@@ -0,0 +1,9 @@
+﻿namespace VictoryGarden_BackEnd.Models
+{
+    public class PostPlot
+    {
+        public int PlantID { get; set; }
+        public string PlantName { get; set; }
+        public int PlotSpace { get; set; }
+    }
+}
