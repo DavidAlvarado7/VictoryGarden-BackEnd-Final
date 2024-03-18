ec8a44fcdd3ba3c814c61cd2272ff5d486f9a41b
Frank Wedlick <jfwedlick@gmail.com>
2024-03-18 19:29:27 -0400

    Added Plants put operation

--- Controllers/PlantsController.cs
+++ Controllers/PlantsController.cs
@@ -31,16 +31,33 @@ namespace VictoryGarden_BackEnd.Controllers
         [HttpGet("{id}")]
         public IActionResult Get([FromRoute] int id)
         {
-            var plants = _victoryGardenDbContext.Plants.Find(id);
+            var plant = _victoryGardenDbContext.Plants.Find(id);
 
-            if (_victoryGardenDbContext.Plants.Contains(plants))
+            if (_victoryGardenDbContext.Plants.Contains(plant))
             {
-                return Ok($"You provided an ID of {id} - {plants.PlantName}.");
+                return Ok($"You provided an ID of {id} - {plant.PlantName}.");
             }
 
             return NotFound();
         }
 
+        // PUT api/<Plot1Controller>/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromRoute] int id, [FromBody] int seedQuantity)
+        {
+            var plant = _victoryGardenDbContext.Plants.Find(id);
 
+            if (_victoryGardenDbContext.Plants.Contains(plant))
+            {
+                plant.Quantity = seedQuantity;
+
+                _victoryGardenDbContext.Plants.Update(plant);
+                _victoryGardenDbContext.SaveChanges();
+
+                return NoContent();
+            }
+
+            return NotFound();
+        }
     }
 }
