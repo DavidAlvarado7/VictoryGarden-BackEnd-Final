3c1c02040fddbf30c490a0410634cc8d2be88a3c
Frank Wedlick <jfwedlick@gmail.com>
2024-03-22 19:44:31 -0400

    fixed journal

--- Models/Journal.cs
+++ Models/Journal.cs
@@ -1,7 +1,8 @@
 ﻿namespace VictoryGarden_BackEnd.Models
 {
-    public class PostJournal
+    public class Journal
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
         public string Gardener { get; set; }
         public string Notes { get; set; }
--- Models/PostJournal.cs
+++ Models/PostJournal.cs
@@ -1,8 +1,7 @@
 ﻿namespace VictoryGarden_BackEnd.Models
 {
-    public class Journal
+    public class PostJournal
     {
-        public int Id { get; set; }
         public DateTime Date { get; set; }
         public string Gardener { get; set; }
         public string Notes { get; set; }
